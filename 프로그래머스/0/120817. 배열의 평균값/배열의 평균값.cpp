564206eb8deabd70202277695df35d7a475847be
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2025-12-22 09:39:00 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    return accumulate(numbers.begin(), numbers.end(), 0.0) / numbers.size();
+}
