d36fe856c850582ff75880777619f8d7339ef7f6
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2025-12-23 09:46:05 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    
+    for(int i = min(a, b); i <= max(a, b); i++)
+    {
+        answer += i;
+    }
+    
+    return answer;
+}
