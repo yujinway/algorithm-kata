c69a3794c9fc44f24ecb6ebbd9af1a8319cb09ef
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2025-12-24 09:27:43 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    
+    for(int i = 0; i < signs.size(); i++)
+    {
+        signs[i] ? answer += absolutes[i] : answer -= absolutes[i];
+    }
+        
+    return answer;
+}
