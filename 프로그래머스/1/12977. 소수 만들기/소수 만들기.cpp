c4c6eef24b918f650ff020e5273cad0661aa6426
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-01-20 10:03:31 +0900

    [level 1] Title: 소수 만들기, Time: 0.36 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num)
+{
+    if(num % 2 == 0 || num < 2)
+    {
+        return false;
+    }
+    if(num == 2)
+    {
+        return true;
+    }
+    
+    for(int i = 3; i * i <= num; i += 2)
+    {
+        if(num % i == 0)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+    
+int solution(vector<int> nums) {
+    int answer = 0;
+    
+    for(int i = 0; i < nums.size() - 2; i++)
+    {
+        for(int j = i + 1; j < nums.size() - 1; j++)
+        {
+            for(int k = j + 1; k < nums.size(); k++)
+            {
+                if(isPrime(nums[i] + nums[j] + nums[k]))
+                {
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
