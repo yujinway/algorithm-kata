06e0e60e0368dc867a83b9448e00a52ac5716235
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-02-06 10:08:08 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int solution(vector<int> arr) {
+    int answer = arr[0];
+    
+    for(int i = 0; i < arr.size(); i++)
+    {
+        answer = lcm(answer, arr[i]);
+    }
+    
+    return answer;
+}
