ec7341877591c9cc7f6139c40f0f159ff49acad0
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-02-09 10:00:07 +0900

    [level 2] Title: 피보나치 수, Time: 0.55 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int solution(int n) {
+    if(n == 0)
+    {
+        return 0;
+    }
+    if(n == 1)
+    {
+        return 1;
+    }
+    
+    vector<int> dp(n + 1);
+    dp[0] = 0;
+    dp[1] = 1;
+    
+    for(int i = 2; i <= n; i++)
+    {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    
+    return dp[n];
+}
