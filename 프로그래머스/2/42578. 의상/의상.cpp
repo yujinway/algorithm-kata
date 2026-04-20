f14ac6634e72fd6eabd0695948321b0b82bf28c7
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-04-20 19:46:53 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -8,15 +8,16 @@ int solution(vector<vector<string>> clothes) {
     int answer = 1;
     unordered_map<string, int> clothes_map;
     
-    for(const auto& cloth : clothes)
+    for(vector<string> cloth : clothes)
     {
         clothes_map[cloth[1]]++;
     }
     
-    for(auto it = clothes_map.begin(); it != clothes_map.end(); it++)
+    for(auto pair : clothes_map)
     {
-        answer *= (it->second + 1);
+        answer *= (pair.second + 1);
     }
     
     return answer - 1;
+
 }
