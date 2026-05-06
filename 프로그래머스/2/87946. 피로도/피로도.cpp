79222c2b555e1deef2cc7588359c9be46756bc23
Yujin Jang <89734178+yujinway@users.noreply.github.com>
2026-05-06 10:43:38 +0900

    [level 2] Title: 피로도, Time: 1.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,28 +1,27 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int answer = 0;
-bool visited[8];
 
-void dfs(vector<vector<int>>& dungeons, int hp, int count)
+void backtrack(vector<vector<int>>& dungeons, vector<int>& visited, int currentFatigue, int count)
 {
     answer = max(answer, count);
     
     for(int i = 0; i < dungeons.size(); i++)
     {
-        if(!visited[i] && hp >= dungeons[i][0])
+        if(!visited[i] && currentFatigue >= dungeons[i][0])
         {
-            visited[i] = true;
-            dfs(dungeons, hp - dungeons[i][1], count + 1);
-            visited[i] = false;
+            visited[i] = 1;
+            backtrack(dungeons, visited, currentFatigue - dungeons[i][1], count + 1);
+            visited[i] = 0;
         }
     }
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    dfs(dungeons, k, 0);
+    vector<int> visited(dungeons.size(), 0);
+    backtrack(dungeons, visited, k, 0);
     return answer;
 }
